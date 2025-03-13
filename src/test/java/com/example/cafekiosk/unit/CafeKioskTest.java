8ad338691cc9686df3b6f8455a5797e769f325d1
aammddkkzxc <ideal0427@naver.com>
2025-03-14 02:32:05 +0900

    수동테스트 VS. 자동화된 테스트 콘솔에 뜬 결과를 보고 사람이 직접 확인 -> 자동화 x

---
+++ src/test/java/com/example/cafekiosk/unit/CafeKioskTest.java
@@ -0,0 +1,18 @@
+package com.example.cafekiosk.unit;
+
+import com.example.cafekiosk.unit.beverage.Americano;
+import org.junit.jupiter.api.Test;
+
+class CafeKioskTest {
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
+
+
+}
