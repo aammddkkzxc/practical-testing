e3aca113fa438d359aea741185b984cd937dfb60
aammddkkzxc <ideal0427@naver.com>
2025-03-21 23:49:26 +0900

    Persistence Layer 테스트 (1)

---
+++ src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
@@ -0,0 +1,24 @@
+package com.example.cafekiosk.spring.api.service.product;
+
+import com.example.cafekiosk.spring.domain.product.Product;
+import com.example.cafekiosk.spring.domain.product.ProductRepository;
+import com.example.cafekiosk.spring.domain.product.ProductSellingStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+
+        return products.stream().map(ProductResponse::of).collect(Collectors.toList());
+    }
+
+}
