3251ccd767eac9b41083ff7174df64d55c238e27
aammddkkzxc <ideal0427@naver.com>
2025-03-25 00:10:15 +0900

    Business Layer 테스트 (1)

---
+++ src/main/java/com/example/cafekiosk/spring/api/controller/order/OrderController.java
@@ -0,0 +1,24 @@
+package com.example.cafekiosk.spring.api.controller.order;
+
+import com.example.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import com.example.cafekiosk.spring.api.service.order.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/api/v1/orders/new")
+    public void createOrder(@RequestBody OrderCreateRequest request) {
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        orderService.createOrder(request, registeredDateTime);
+    }
+
+}
--- src/test/java/com/example/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/com/example/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -13,7 +12,6 @@ import static com.example.cafekiosk.spring.domain.product.ProductSellingStatus.*
 import static com.example.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 //@SpringBootTest
@@ -64,4 +62,37 @@ class ProductRepositoryTest {
         ;
     }
 
+    @DisplayName("상품 번호 리스트로 상품들을 조회한다")
+    @Test
+    void findAllByProductNumberIn() {
+        //given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        //when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
+
+        //then
+        assertThat(products).hasSize(2);
+    }
 }
