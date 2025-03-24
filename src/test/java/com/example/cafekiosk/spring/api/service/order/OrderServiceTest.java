5486264bc2424a4b053e2c2adc2897b617e2d75b
aammddkkzxc <ideal0427@naver.com>
2025-03-25 01:21:09 +0900

    Business Layer 테스트 (2) - productNumbers가 ["001", "001"]인 경우, findAllByProductNumberIn은 단 하나의 Product 객체를 반환 - @Transactional 포함하는 어노테이션인지 주의

--- src/test/java/com/example/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/com/example/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -2,9 +2,12 @@ package com.example.cafekiosk.spring.api.service.order;
 
 import com.example.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import com.example.cafekiosk.spring.api.service.order.response.OrderResponse;
+import com.example.cafekiosk.spring.domain.order.OrderRepository;
+import com.example.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import com.example.cafekiosk.spring.domain.product.Product;
 import com.example.cafekiosk.spring.domain.product.ProductRepository;
 import com.example.cafekiosk.spring.domain.product.ProductType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +32,20 @@ class OrderServiceTest {
     private ProductRepository productRepository;
 
     @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
     private OrderService orderService;
+
+    @AfterEach
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -70,6 +86,37 @@ class OrderServiceTest {
                 .sellingStatus(SELLING)
                 .name("메뉴 이름")
                 .build();
+    }
+
+    @DisplayName("중복되는 상품번호 리스트로 중문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() throws Exception{
+        //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 2000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000)
+                );
     }
 
 }
