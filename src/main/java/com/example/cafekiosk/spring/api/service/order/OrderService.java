dc28d88bc3d2a07a86a4947f5fb1bdb49d0b6455
aammddkkzxc <ideal0427@naver.com>
2025-03-25 23:05:56 +0900

    Business Layer 테스트 (3) - 고민해야할 문제 아래 두개는 관점이 달라 두번 체크할 필요성이 있다   - 서비스에서 진행한 체크는 주문 생성 로직을 수행하다가 스톡 재고에 대한 차감을 시도하는과정   - 스톡 자체의 메서드 같은 경우는 스톡은 밖에 서비스가 어떻게 돼 있는지 전혀 모름 -> 단위테스트, 항상 보장해줘야 함 - 서비스 테스트 시 @SpringBootTest + @Transactional 주의점   - 테스트 코드에서 @Transactional을 사용하면 비즈니스 로직이 트랜잭션을 포함하고 있는 것처럼 착각할 수 있다.

--- src/main/java/com/example/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/com/example/cafekiosk/spring/api/service/order/OrderService.java
@@ -6,25 +6,34 @@ import com.example.cafekiosk.spring.domain.order.Order;
 import com.example.cafekiosk.spring.domain.order.OrderRepository;
 import com.example.cafekiosk.spring.domain.product.Product;
 import com.example.cafekiosk.spring.domain.product.ProductRepository;
+import com.example.cafekiosk.spring.domain.product.ProductType;
+import com.example.cafekiosk.spring.domain.stock.Stock;
+import com.example.cafekiosk.spring.domain.stock.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final StockRepository stockRepository;
 
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
 //        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
         List<Product> products = findProductsBy(productNumbers);
+
+        deductStockQuantities(products);
 
         Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
@@ -39,6 +48,44 @@ public class OrderService {
         return productNumbers.stream()
                 .map(productMap::get)
                 .collect(Collectors.toList());
+    }
+
+    private void deductStockQuantities(List<Product> products) {
+        List<String> stockProductNumbers = extractStockProductNumbers(products);
+
+        Map<String, Stock> stockMap = createStockMapBy(stockProductNumbers);
+        Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
+
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+
+            //아래 두개는 관점이 달라 두번 체크할 필요성이 있다
+            //서비스에서 진행한 체크는 주문 생성 로직을 수행하다가 스톡 재고에 대한 차감을 시도하는과정
+            if (stock.isQuantityLessThan(quantity)) {
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            //스톡 자체의 메서드 같은 경우는 스톡은 밖에 서비스가 어떻게 돼 있는지 전혀 모름 -> 단위테스트, 항상 보장해줘야 함
+            stock.deductQuantity(quantity);
+        }
+    }
+
+    private static List<String> extractStockProductNumbers(List<Product> products) {
+        return products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .collect(Collectors.toList());
+    }
+
+    private Map<String, Stock> createStockMapBy(List<String> stockProductNumbers) {
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        return stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
+    }
+
+    private static Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+        return stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
     }
 
 }
