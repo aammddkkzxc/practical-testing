0b9a3fd54ea6c075292614af1a00f336950709c4
aammddkkzxc <ideal0427@naver.com>
2025-03-26 19:26:25 +0900

    Presentation Layer 테스트 (1) - 상품 등록시 상품 번호를 1씩 증가시키는 정책일 시 동시성 이슈에 대해 고민할 필요가 있다.   - 빈도수가 엄청 높은 편은 아니라고 예상할 수 있다.   - 시스템이 알아서 재시도 해서 통과할 수 있게 할 수 있다.   - 아예 정책을 바꿔서uuid같은 것을 쓰는 방법도 고려해 볼 수 있다. - @Transactional(readOnly = true)   - jpa성능향상   - cqrs

--- src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
@@ -1,24 +1,49 @@
 package com.example.cafekiosk.spring.api.service.product;
 
+import com.example.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import com.example.cafekiosk.spring.domain.product.Product;
 import com.example.cafekiosk.spring.domain.product.ProductRepository;
 import com.example.cafekiosk.spring.domain.product.ProductSellingStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
         return products.stream().map(ProductResponse::of).collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 
 }
