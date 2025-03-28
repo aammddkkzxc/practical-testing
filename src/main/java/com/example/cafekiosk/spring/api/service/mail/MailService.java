4f377f0d456670c05d81a82c66c8ededcc4b0c06
aammddkkzxc <ideal0427@naver.com>
2025-03-28 19:26:30 +0900

    @Mock, @Spy, @InjectMocks - 목 객체만 만들고 stubbing을 해주지 않는다면 기본 정책을 따른다(null, 0 등등 리턴) - mock, spy 차이

--- src/main/java/com/example/cafekiosk/spring/api/service/mail/MailService.java
+++ src/main/java/com/example/cafekiosk/spring/api/service/mail/MailService.java
@@ -18,13 +18,21 @@ public class MailService {
     public boolean sendMail(String fromEmail, String toEmail, String subject, String content) {
         boolean result = mailSendClient.sendEmail(fromEmail, toEmail, subject, content);
         if (result) {
-            mailSendHistoryRepository.save(MailSendHistory.builder()
+            //디버거로 stubbing 기본 정책 확인
+            MailSendHistory history = mailSendHistoryRepository.save(MailSendHistory.builder()
                     .fromEmail(fromEmail)
                     .toEmail(toEmail)
                     .subject(subject)
                     .content(content)
                     .build()
             );
+
+            //spy mock 비교 호출
+            mailSendClient.a();
+            mailSendClient.b();
+            mailSendClient.c();
+            mailSendClient.d();
+
             return true;
         }
 
